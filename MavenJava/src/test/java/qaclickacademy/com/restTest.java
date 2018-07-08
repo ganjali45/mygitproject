a5a79a86c9e7a85706a3d2a44e13e966dc376916
U6022942 <U6022942@U6022942-TPL-A.ten.thomsonreuters.com>
2018-07-09 01:33:16 +0530

    Test Commit

---
+++ MavenJava/src/test/java/qaclickacademy/com/restTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.com;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class restTest {
+
+	
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+}
---
+++ MavenJava/src/test/java/qaclickacademy/com/seleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.com;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+
+	}
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+}
